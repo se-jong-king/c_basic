9613868a1b1407e46cae8f86dab075b8313a5e37
sejong <112914777+se-jong-king@users.noreply.github.com>
2022-09-19 10:48:32 +0900

    finished ch.01

---
+++ chapter02/c_printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ *  제어 문자
+ *  + \n : 한줄 개행
+ *  + \t : 탭 (8칸 스페이스)
+ *
+ * 
+*/
+
+int main(void)
+{
+    /* code */
+    printf("HELLO\n");
+    printf("SeJong \t Noh");
+
+    return 0;
+}
