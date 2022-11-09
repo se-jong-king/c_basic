4feb8087e01aa435660d9b2e72fc3a772b92e0d4
sejong <112914777+se-jong-king@users.noreply.github.com>
2022-11-09 10:51:39 +0900

    Add files via upload

---
+++ 000.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(void) 
+{
+    int num = 57256;
+    int num1 = num / 10000;
+    int num2 = (num - num1*10000) / 1000;
+    int num3 = (num / 100;
+    int num4 = num / 10;
+    int num5 = num / 1;
+
+    for (int i = 1; i < ; i++)
+    {
+        /* code */
+    }
+    
+    int total = 0;
+
+    printf("57256 / 10000 = %d", num);
+    
+    printf("ÃÑÇÕ = %d", total);
+    printf("%d, %d, %d, %d, %d", num1, num2, num3, num4, num5);
+}
